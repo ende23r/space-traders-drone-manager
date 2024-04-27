cd4dbaa8db670e79a86d39e0a228ab306af51506
Eric <ericandow@gmail.com>
2024-04-27 19:48:26 -0400

    Prep app for green field development
    - Remove css
    - Set up MUI Grid layout
    - Stub out API call to list current ships

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState, useRef  } from 'react'
+// import  Box  from '@mui/material/Box';
 import  Button  from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
-import './App.css'
+// import Grid from '@mui/material/Grid'; // Grid version 1
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 
-async function checkBearerToken(token:string) {
+async function checkBearerToken(token: string) {
   const options = {
     headers: {
       Authorization: `Bearer ${token}`
@@ -15,9 +17,20 @@ async function checkBearerToken(token:string) {
   return response.ok;
 }
 
+async function updateShipInfo(token: string) {
+  const options = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  };
+  const response = await fetch("https://api.spacetraders.io/v2/my/ships", options);
+  const responseData = await response.json();
+  console.log(responseData);
+  return response.ok;
+}
 
 function App() {
-  const [_bearerToken, registerBearerToken] = useState("")
+  const [bearerToken, registerBearerToken] = useState("")
   const textRef = useRef({value: ""});
 
   const submit = async () => {
@@ -40,7 +53,18 @@ function App() {
         <Button variant="contained" onClick={submit}>Register Bearer Token</Button>
 </div>
       <div>
-        {/*<!-- TODO: basic actions -->*/}
+        <Grid container spacing={1}>
+          <Grid xs={6}>
+            <p>
+              Ship List
+              <Button variant="contained" onClick={() => updateShipInfo(bearerToken)}>Update Ship Info</Button>
+            </p>
+            <div>
+
+            </div>
+          </Grid>
+          <Grid xs={6}>Details and Actions</Grid>
+        </Grid>
       </div>
     </>
   )
